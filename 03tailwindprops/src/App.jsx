d5bb80f5b7109cf6975a63b30bf9f4ba09ddc9c4
Vijayalaxmi Shirnal <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2024-06-11 23:54:10 +0530

    Update App.jsx

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,7 +7,7 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
   let myObj = {
-    username: "hitesh",
+    username: "Vijayalaxmi",
     age: 21
   }
   let newArr = [1, 2, 3]
@@ -16,7 +16,7 @@ function App() {
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
       <Card username="chaiaurcode" btnText="click me" />
-      <Card username="hitesh" />
+      <Card username="Vijayalaxmi" />
     </>
   )
 }
